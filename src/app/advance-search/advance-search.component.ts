8dde44ef6cce25fd969838f4389cbc8f7a6280ad
shahidaslam0313 <shahid.aslam@brainplow.com>
2019-03-20 11:43:30 +0500

    lakjsdlfkjas

--- src/app/advance-search/advance-search.component.ts
+++ src/app/advance-search/advance-search.component.ts
@@ -435,6 +435,8 @@ export class AdvanceSearchComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
 
+
+
     if (localStorage.getItem('currentadmin')) {
       this.adminlogin = localStorage.getItem('currentadmin')
     }
